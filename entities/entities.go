2c20c726101deeff4a13fc2979e0e8c9b3e59491
ray <raycole@MACBOOKPRO-C66E.local>
2013-07-15 23:06:16 -0400

    Element definitions - Added more block ids - Added entity ids - Added item ids

---
+++ entities/entities.go
@@ -0,0 +1,78 @@
+package entities
+
+const (
+	Item = 1
+	XPOrb = 2
+	
+	// Immobile
+	LeashKnot = 8
+	Painting = 9
+	ItemFrame = 18
+	
+	// Projectiles
+	Arrow = 10
+	Snowball = 11
+	Fireball = 12
+	SmallFireball = 13
+	ThrownEnderpearl = 14
+	EyeOfEnderSignal = 15
+	ThrownPotion = 16
+	ThrownExpBottle = 17
+	WitherSkull = 19
+	FireworksRocketEntity = 22
+	
+	// Blocks
+	PrimedTNT = 20
+	FallingSand = 21
+	
+	// Vehicles
+	Boat = 41
+	MinecartRideable = 42
+	MinecartChest = 43
+	MinecartFurnace = 44
+	MinecartTNT = 45
+	MinecartHopper = 46
+	MinecartSpawner = 47
+	
+	// Generic
+	Mob = 48
+	Monster = 49
+
+	// Hostile mobs
+	Creeper = 50
+	Skeleton = 51
+	Spider = 52
+	Giant = 53
+	Zombie = 54
+	Slime = 55
+	Ghast = 56
+	PigZombie = 57
+	Enderman = 58
+	CaveSpider = 59
+	Silverfish = 60
+	Blaze = 61
+	LavaSlime = 62
+	EnderDragon = 63
+	WitherBoss = 64
+	Witch = 66
+
+	// Passive mobs
+	Bat = 65
+	Pig = 90
+	Sheep = 91
+	Cow = 92
+	Chicken = 93
+	Squid = 94
+	Wolf = 95
+	MushroomCow = 96
+	SnowMan = 97
+	Ozelot = 98
+	VillagerGolem = 99
+	EntityHorse = 100
+	
+	// NPCs
+	Villager = 120
+
+	// Other
+	EnderCrystal = 200
+)
